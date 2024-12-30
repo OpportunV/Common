d35cc3a055a588c2baf9b75ac5c05134b21885b7
OpportunV <RsTGear@gmail.com>
2024-12-30 21:19:59 +0300

    Moved helpers from AoC2023

---
+++ Common/Extensions/EnumerableExt.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace Common.Extensions;
+
+public static class EnumerableExt
+{
+    public static TSource Product<TSource>(this IEnumerable<TSource> source) where TSource : INumber<TSource>
+    {
+        return source.Aggregate(TSource.MultiplicativeIdentity, (accumulate, number) => accumulate * number);
+    }
+
+    public static IEnumerable<TSource> Print<TSource>(this IEnumerable<TSource> source, string separator = "\n")
+    {
+        var list = source.ToList();
+        if (list.Count == 0)
+        {
+            return list;
+        }
+
+        foreach (var value in list)
+        {
+            Console.Write($"{value}{separator}");
+        }
+
+        return list;
+    }
+}
---
+++ Common/Models/Range.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+namespace Common.Models;
+
+public record Range<T> where T : INumber<T>
+{
+    public T Start { get; }
+
+    public T End { get; }
+    
+    public T Length { get; }
+
+    public bool Ascending { get; }
+
+    public Range(T start, T end)
+    {
+        Start = start;
+        End = end;
+        Ascending = End > Start;
+        Length = End - Start;
+    }
+
+    public IEnumerable<T> Enumerate()
+    {
+        for (var i = Start; i < End; i++)
+        {
+            yield return i;
+        }
+    }
+
+    public Range<T> Intersection(Range<T> other)
+    {
+        return new Range<T>(T.Max(Start, other.Start), T.Min(End, other.End));
+    }
+
+    public bool StartsBefore(Range<T> other)
+    {
+        return Start < other.Start;
+    }
+
+    public bool EndsBefore(Range<T> other)
+    {
+        return End < other.End;
+    }
+}
