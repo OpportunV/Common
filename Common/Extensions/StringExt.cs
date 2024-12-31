3d3ee3fe3a1e9cc34d64dc13e7bdac84c87434bc
OpportunV <RsTGear@gmail.com>
2024-12-31 21:51:49 +0300

    String ext to parse input

---
+++ Common/Extensions/StringExt.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Common.Extensions;
+
+public static class StringExt
+{
+    private static readonly Regex _numbers = new(@"-?\d+");
+    private static readonly Regex _words = new(@"\w+");
+
+    public static List<T> GetNumbers<T>(this string input) where T : INumber<T>
+    {
+        return _numbers.Matches(input).Select(match => T.Parse(match.Value, CultureInfo.InvariantCulture)).ToList();
+    }
+
+    public static List<string> GetWords(this string input)
+    {
+        return _words.Matches(input).Select(match => match.Value).ToList();
+    }
+}
