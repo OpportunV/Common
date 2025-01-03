cd954a951982681609f5baf0d340ca6af267d916
OpportunV <RsTGear@gmail.com>
2025-01-04 00:32:07 +0300

    Added pick's and shoelace formulas

--- Common/Helpers/Calculator.cs
+++ Common/Helpers/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using Common.Models;
 
 namespace Common.Helpers;
 
@@ -20,5 +21,48 @@ public static class Calculator
 
             (a, b) = (b, a % b);
         }
+    }
+
+    /// <summary>
+    /// Calculates polygon's area using
+    /// <a href="https://11011110.github.io/blog/2021/04/17/picks-shoelaces.html">Shoelace (Gauss) formula.</a>
+    /// </summary>
+    /// <param name="corners">Ordered vertexes of the polygon.</param>
+    /// <returns>Polygon's area.</returns>
+    public static long PolygonArea(List<GridPos2d> corners)
+    {
+        var area = 0L;
+        foreach (var (prev, cur) in corners.Zip(corners[1..].Append(corners[0])))
+        {
+            area += (long)(cur.Row - prev.Row) * (cur.Col + prev.Col) / 2;
+        }
+
+        return Math.Abs(area);
+    }
+
+    /// <summary>
+    /// Calculates amount of the integer points inside a polygon using
+    /// <a href="https://11011110.github.io/blog/2021/04/17/picks-shoelaces.html">Pick's formula.</a>
+    /// </summary>
+    /// <param name="area">Total polygon's area.</param>
+    /// <param name="boundary">Amount of points on the polygon's boundary.</param>
+    /// <returns>Amount of the integer points inside a polygon</returns>
+    public static long PolygonInteriorPoints(long area, long boundary)
+    {
+        return area - boundary / 2 + 1;
+    }
+
+    /// <summary>
+    /// Combines <see cref="PolygonArea"/> and <see cref="PolygonInteriorPoints"/>
+    /// to get the total amount of the polygon's integer points. 
+    /// </summary>
+    /// <param name="corners">Ordered vertexes of the polygon.</param>
+    /// <param name="boundary">Amount of points on the polygon's boundary.</param>
+    /// <returns>Total amount of the polygon's integer points</returns>
+    public static long PolygonTotalPoints(List<GridPos2d> corners, long boundary)
+    {
+        var area = PolygonArea(corners);
+        var interior = PolygonInteriorPoints(area, boundary);
+        return interior + boundary;
     }
 }
