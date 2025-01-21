5295eb8d8e6470f79d90061999db72238c0fdef7
OpportunV <RsTGear@gmail.com>
2025-01-21 19:08:49 +0300

    Added GetOrAdd and TryUpdate dict ext

---
+++ Common/Extensions/DictionaryExt.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Common.Extensions;
+
+public static class DictionaryExt
+{
+    public static TValue? GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue? value)
+        where TKey : notnull
+    {
+        ref var val = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+        if (exists)
+        {
+            return val;
+        }
+
+        val = value;
+        return value;
+    }
+
+    public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        where TKey : notnull
+    {
+        ref var val = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+        if (Unsafe.IsNullRef(ref val))
+        {
+            return false;
+        }
+
+        val = value;
+        return true;
+    }
+}
