1449954ff73a3bdfc4a9410b81b88bea1478414e
OpportunV <RsTGear@gmail.com>
2025-12-05 20:16:34 +0300

    Added Contains and Union methods to Range<T> class

--- Common/Models/Range.cs
+++ Common/Models/Range.cs
@@ -2,6 +2,11 @@
 
 namespace Common.Models;
 
+/// <summary>
+/// Represents a range that has start and end indexes.
+/// Start is inclusive, end is exclusive.
+/// </summary>
+/// <typeparam name="T">Type of the range item. Must implement <see cref="INumber{T}"/></typeparam>
 public record Range<T> where T : INumber<T>
 {
     public T Start { get; }
@@ -46,5 +51,20 @@ public record Range<T> where T : INumber<T>
     public bool EndsBefore(Range<T> other)
     {
         return End < other.End;
+    }
+
+    public bool Contains(T item)
+    {
+        return Start <= item && item <= End;
+    }
+
+    public Range<T> Union(Range<T> other)
+    {
+        if (!Intersects(other))
+        {
+            throw new ArgumentException("Cannot get a union of two ranges with no intersection.");
+        }
+
+        return new Range<T>(T.Min(Start, other.Start), T.Max(End, other.End));
     }
 }
